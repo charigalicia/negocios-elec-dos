9c2447dbda94c1b7fae09226fb5977ff404ca973
Maria Galicia <rosario.galen7@gmail.com>
2025-04-14 15:14:29 -0600

    Panel admin completo

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,8 +1,87 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { backendUrl, currency } from '../App'
+import { toast } from 'react-toastify'
 
-const List = () => {
+const List = ({token}) => {
+  const [list, setList]=useState([])
+
+  const fetchList=async()=>{
+    try {
+      const response =await axios.get(backendUrl+'/api/product/list')
+      if (response.data.success) {
+        setList(response.data.products);
+      }else{
+        toast.error(response.data.message)
+      }
+      
+      
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      
+      
+    }
+
+  }
+
+  const removeProduct =async(id)=>{
+    try {
+      const response = await axios.post(backendUrl+'/api/product/remove', {id}, {headers:{token}})
+
+      if (response.data.success) {
+        toast.success(response.data.message)
+        await fetchList();
+        
+      }else{
+        toast.error(response.data.message)
+        console.log(error);
+        toast.error(error.message)
+      }
+
+    } catch (error) {
+      
+    }
+
+  }
+useEffect(()=>{
+  fetchList()
+
+},[])
+
   return (
-    <div></div>
+    <>
+       <p className='mb-2'>Todos los productos</p>
+       <div>
+
+        {/* -------Titulo tabla */}
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sml'>
+          <b>Imagen</b>
+          <b>Nombre</b>
+          <b>Categoria</b>
+          <b>Precio</b>
+          <b className='text-center'>Accion</b>
+        </div>
+
+        {/* -----Lista de productos */}
+        {
+          list.map((item,index)=>(
+            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+              <img className='w-12' src={item.image[0]} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{currency}{item.price}</p>
+              <p onClick={()=> removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg' >X</p>
+
+            </div>
+
+
+          ))
+        }
+       </div>
+
+    </>
   )
 }
 
